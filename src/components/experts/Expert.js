b355267dc36727143a5d4f484e4c72f11dfc1d43
Devipad Das <devipad16@gmail.com>
2021-02-17 09:36:00 +0530

    update: Experts profiles

--- src/components/experts/Expert.js
+++ src/components/experts/Expert.js
@@ -11,40 +11,41 @@ function Expert() {
             <h2>Meet our Experts</h2>
             <div className='expert-crd-cont'>
                 <div className='expert-crd'>
-                    <img src='https://d2homsd77vx6d2.cloudfront.net/avatars/2020/10/01/profiletwo.ju14vR8p6I.jpg' alt='profile' />
-                    <h3>Arya sharma</h3>
+                    <img src='https://perfeth.com/wp-content/uploads/2017/11/Mor_Sagmon_mentor.jpeg' alt='profile' />
+                    <h3>Mor Sagmon</h3>
+                    <h4>Software Engineer</h4>
+                    <p>Managing Director of MorSagmon.com Business/ Tech Expert</p>
+                </div>
+                <div className='expert-crd'>
+                    <img src='https://perfeth.com/wp-content/uploads/2020/09/isha.jpg' alt='profile' />
+                    <h3>Isha Nema</h3>
                     <h4>Software Engineer</h4>
                     <p>Software engineer with experience in Python / algorithm / database / agile.</p>
                 </div>
                 <div className='expert-crd'>
-                    <img src='https://i.redd.it/6onq25y0sh311.jpg' alt='profile' />
-                    <h3>raghu nadh</h3>
+                    <img src='https://perfeth.com/wp-content/uploads/2020/09/lauren.png' alt='profile' />
+                    <h3>Lauren Hasson</h3>
                     <h4>Software Engineer</h4>
-                    <p>Software engineer with experience in Python / algorithm / database / agile.</p>
+                    <p>Software developer at a fintech firm and founder of Develop Her</p>
                 </div>
                 <div className='expert-crd'>
-                    <img src='https://images.pexels.com/photos/1310522/pexels-photo-1310522.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt='profile' />
-                    <h3>anuja shinde</h3>
+                    <img src='https://perfeth.s3.ap-south-1.amazonaws.com/academy/static/shivam.jpeg' alt='profile' />
+                    <h3>Shivam Sethi</h3>
                     <h4>Software Engineer</h4>
-                    <p>Software engineer with experience in Python / algorithm / database / agile.</p>
+                    <p>Lead Engineer, Mzaalo. React and System designs enthusiast. Lifelong developer. Domains - OTT, Blockchain.</p>
                 </div>
                 <div className='expert-crd'>
-                    <img src='https://gust.com/marketing_assets/launch/blog/authors/keyvanfirouzi-01402d692d2a80c40598db2247edaf57f4b94e537263c0b074dee462b4eb3268.png' alt='profile' />
-                    <h3>suraj pandey</h3>
+                    <img src='https://perfeth.com/wp-content/uploads/2020/09/tushar.jpeg' alt='profile' />
+                    <h3>Tushar Bhatnagar</h3>
                     <h4>Software Engineer</h4>
-                    <p>Software engineer with experience in Python / algorithm / database / agile.</p>
+                    <p>Software Engineer with expertise in AI/ML and cofounder of AIotize</p>
                 </div>
                 <div className='expert-crd'>
-                    <img src='https://static4.depositphotos.com/1005893/311/i/950/depositphotos_3110740-stock-photo-good-looking-asian.jpg' alt='profile' />
-                    <h3>Kapil ojha</h3>
+                    <img src='https://perfeth.s3.ap-south-1.amazonaws.com/profile-pictures/9f8f4eb0-615a-4e08-9be3-cd5de10b7abf.jpg' alt='profile' />
+                    <h3>Shivam Verma</h3>
                     <h4>Software Engineer</h4>
-                    <p>Software engineer with experience in Python / algorithm / database / agile.</p>
-                </div>
-                <div className='expert-crd'>
-                    <img src='https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxMzkzMDkyfHxlbnwwfHx8&auto=format&fit=crop&w=400&q=60' alt='profile' />
-                    <h3>Isha nema</h3>
-                    <h4>Software Engineer</h4>
-                    <p>Software engineer with experience in Python / algorithm / database / agile.</p>
+                    <p>Senior software engineer - Google ,speaker at Global Blockchain Foundation 2018 ,speaker at PyCon
+                    India 2014</p>
                 </div>
                 
             </div>
